b680c8513807f760e52be17af2a9d9d049d06be7
Dinesh Kumar <66234432+Dinesh123207@users.noreply.github.com>
2023-09-17 14:38:22 +0530

    Starting

---
+++ week1.1.js
@@ -0,0 +1,36 @@
+//let blackbox = {
+  //  name: 'rohan',
+    //yearold: 32
+
+//}
+//console.log(blackbox)
+//a=5
+//console.log(a);
+
+///////////////////////
+
+//Sum(1,100)
+//function sumofall(start, end) {
+  //  let sum =0;
+    //for(let i = start; i<end;i++){
+      //  sum += i;
+    //}
+    //return(sum);
+//}
+    
+    
+//sum = sumofall(1,100);
+//console.log(sum);
+
+// star pattern
+function star(n1){
+    let n =0;
+    for(let i=n;i<n;i++){
+        for(let j= i+2;j<i;j++);
+
+    }
+    console.log('*');
+    return(n);
+}
+p=star(7);
+console.log(p);
