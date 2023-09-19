84826e8e04f911185217d10ae97dd706fd8db12c
Dinesh Kumar <66234432+Dinesh123207@users.noreply.github.com>
2023-09-19 19:56:34 +0530

    datatypes1

---
+++ data_types.js
@@ -0,0 +1,15 @@
+// String.
+//Number.
+//Bigint.
+//Boolean.
+//Undefined.
+//Null.
+//Symbol.
+//Object
+var myName = 'Dinesh';
+var myAge =56;
+console.log(myName);
+console.log(myAge);
+console.log(typeof(myAge));
+console.log(typeof(myName));
+
